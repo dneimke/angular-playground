8c0936e56e859e22b5ba7417dbf18cec71ad5e24
Darren Neimke <darren.neimke@live.com>
2017-11-26 13:57:06 +1030

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+export class MessageService {
+  createMessage(): Observable<string> {
+    return Observable.create("Hello World");
+  }
+}
+
+@Injectable()
+export class Multiplier {
+  constructor(private messageService: MessageService) { }
+
+  getMessages(quantity: number): string[] {
+    this.messageService.createMessage()
+        .subscribe();
+    // var messages = new Array(quantity)
+    //   .map(function(item, index){ return message;})
+    // return messages;
+
+    return [];
+  }
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '{{title}}',
+  styles: []
 })
-export class AppComponent {
+export class AppComponent { 
+
   title = 'app';
+
+  constructor(private multiplier: Multiplier) {
+    this.title = multiplier.getMessages(3).join('<br />');
+  }
 }
